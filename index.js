83f7d8c49ba0ebf2e9432fff7418b4530eabf54b
chzellweger <christian.zellweger@derbund.ch>
2022-11-27 16:45:21 +0100

    create new page

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const colorCombinations = [[]]
+const colorCombinations = [
+  ['#00A0B0'],
+  ['#6A4A3C'],
+  ['#CC333F'],
+  ['#EB6841'],
+  ['#EDC951'],
+]
 
-document.onload(function name(params) {})
+window.addEventListener('load', function () {
+  console.log('hello')
+  const colorCombination = colorCombinations[Math.floor(Math.random() * 4)]
+  document.querySelector('body').style.backgroundColor = colorCombination[0]
+})
