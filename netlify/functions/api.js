5186e0fd4984da7a0d7fe7de99908c60640722e0
chzellweger <christian.zellweger@derbund.ch>
2022-12-22 01:38:13 +0100

    fix npm

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,28 +1,29 @@
 
-import fetch from 'node-fetch'
+// import fetch from 'node-fetch'
 
 exports.handler = async (event, context) => {
 
-const API_ENDPOINT = 'https://www.dwds.de/r'
-const query = '%40Er+WITH+%24.%3D0+%26%26+%40ist+WITH+%24.%3D1'
-const corpus = 'kern'
-const dateStart = '1900'
-const dateEnd = '1999'
-const genre = 'Wissenschaft'
-const format = 'full'
-const sort = 'random'
-const limit = '100'
-const view = 'csv'
+// const API_ENDPOINT = 'https://www.dwds.de/r'
+// const query = '%40Er+WITH+%24.%3D0+%26%26+%40ist+WITH+%24.%3D1'
+// const corpus = 'kern'
+// const dateStart = '1900'
+// const dateEnd = '1999'
+// const genre = 'Wissenschaft'
+// const format = 'full'
+// const sort = 'random'
+// const limit = '100'
+// const view = 'csv'
 
-const url = `${API_ENDPOINT}/?q=${query}&corpus=${corpus}&date-start=${dateStart}&date-end=${dateEnd}&genre=${genre}&format=${format}&sort=${sort}&limit=${limit}&view=${view}`
+// const url = `${API_ENDPOINT}/?q=${query}&corpus=${corpus}&date-start=${dateStart}&date-end=${dateEnd}&genre=${genre}&format=${format}&sort=${sort}&limit=${limit}&view=${view}`
 
   let response
+
   try {
-    return 'hello world from api'
+    response = 'hello world from api'
 
 
-    response = await fetch(url).then(res.text()).then(json => console.log(json))
-    console.log(response)
+    // response = await fetch(url).then(res.text()).then(json => console.log(json))
+    // console.log(response)
     // handle response
   } catch (err) {
     return {
